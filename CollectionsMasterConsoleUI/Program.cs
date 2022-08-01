ffbebe67277dc01803c672822b924df16b38d00f
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-01 03:27:32 -0500

    initial commit

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -14,19 +14,19 @@ namespace CollectionsMasterConsoleUI
             #region Arrays
             //TODO: Create an integer Array of size 50
 
-            var num = new int[50];
+            var myArr = new int[50];
             //TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
 
-            Populater(num);
+            Populater(myArr);
             //TODO: Print the first number of the array
             Console.WriteLine("First num");
-            Console.WriteLine($"{num[0]}");
+            Console.WriteLine($"{myArr[0]}");
             //TODO: Print the last number of the array
             Console.WriteLine("Last num");
-            Console.WriteLine(num[num.Length - 1]);
+            Console.WriteLine($"{myArr[myArr.Length - 1]}");
             Console.WriteLine("All Numbers Original");
             //UNCOMMENT this method to print out your numbers from arrays or lists
-            NumberPrinter(num);
+            NumberPrinter(myArr);
             Console.WriteLine("-------------------");
 
             //TODO: Reverse the contents of the array and then print the array out to the console.
@@ -34,19 +34,19 @@ namespace CollectionsMasterConsoleUI
             /*  1) First way, using a custom method => Hint: Array._____(); 
                 2) Second way, Create a custom method (scroll to bottom of page to find ⬇⬇⬇)
             */
-            Array.Reverse(num);
+            Array.Reverse(myArr);
             Console.WriteLine("All Numbers Reversed:");
-            NumberPrinter(num);
+            NumberPrinter(myArr);
 
             Console.WriteLine("---------REVERSE CUSTOM------------");
-            Array.Reverse(num);
+            Array.Reverse(myArr);
 
             Console.WriteLine("-------------------");
 
 
             //TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
             Console.WriteLine("Multiple of three = 0: ");
-            ThreeKiller(num);
+            ThreeKiller(myArr);
 
             Console.WriteLine("-------------------");
 
@@ -54,8 +54,8 @@ namespace CollectionsMasterConsoleUI
             /*      Hint: Array.____()      */
             Console.WriteLine("Sorted numbers:");
 
-            Array.Sort(num);
-            NumberPrinter(num);
+            Array.Sort(myArr);
+            NumberPrinter(myArr);
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
@@ -121,7 +121,7 @@ namespace CollectionsMasterConsoleUI
         }
 
         private static void ThreeKiller(int[] num)
-        { foreach (int = 0; i < num.Length; i++) 
+        { for (int = 0; i < num.Length; i++) 
             {
                 if (num[i] % 3 == 0)
                 {
@@ -131,8 +131,8 @@ namespace CollectionsMasterConsoleUI
             foreach (int i in num) {
 
                 Console.WriteLine(i);
+                }
             }
-        }
         private static void OddKiller(List<int> numberList)
         {
             var tarVal = new List<int>();
@@ -183,20 +183,19 @@ namespace CollectionsMasterConsoleUI
             for (int i = 0; i < numbers.Length; i++)
             {
                 Random rng = new Random();
-                numbers[i] = rng.Next(51);
+                numbers[i] = rng.Next(0, 50);
             }
-
 
             private static void ReverseArray(int[] array)
             {
-                //create new array
+                
                 int[] newArray = new int[50];
-                //reverse values
+              
                 for (int i = 0; i < array.Length; i++)
                 {
                     newArray[i] = array[array.Length - 1 - i];
                 }
-                // update arguments with arrays     
+                     
                 for (int i = 0; i < array.Length; i++)
                 {
                     array[i] = newArray[i];
